9cbaeef8b7f540061ff2ed2bc9232e81e5fe45ea
Caroline Joyce <carol96joyce@gmail>
2025-07-24 13:15:25 +0300

    Add my file to existing GitHub repo

--- typeConversion.js
+++ typeConversion.js
@@ -1,3 +1,21 @@
 let var1="20"
 let var2="30"
 console.log(parseFloat(var1)*parseFloat(var2))
+let str='42'
+let num=0
+let num1=7
+let str1=''
+let bool=true
+let num2=123
+let bool1=Boolean(str)
+let bool2=Boolean(num)
+let bool3=Boolean(num1)
+let bool4=Boolean(str1)
+let str2=String(bool)
+let str3=String(num2)
+console.log(bool1)
+console.log(bool2)
+console.log(bool3)
+console.log(bool4)
+console.log(str2)
+console.log(str3)
