34151bd808179e5e9025612956568692e6c544a5
Caroline Joyce <carol96joyce@gmail>
2025-07-24 11:17:09 +0300

    Add file to existing Git repo

---
+++ switchStatement.js
@@ -0,0 +1,22 @@
+let numGrade=90
+let letterGrade
+switch(true){
+    case numGrade>=90 && numGrade<=100:
+        letterGrade="A"
+        break
+    case numGrade>=80 && numGrade<=89:
+        letterGrade="B"
+        break
+    case numGrade>=70 && numGrade<=79:
+        letterGrade="C"
+        break
+    case numGrade>=60 && numGrade<=69:
+        letterGrade="D"
+        break
+    case numGrade>=0 && numGrade<=59:
+        letterGrade="F"
+        break
+    default:
+        letterGrade="Invalid grade"
+}
+console.log(letterGrade)
