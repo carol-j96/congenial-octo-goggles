d17203dd25ce39641197f37037e08a6bdecb336e
Caroline Joyce <carol96joyce@gmail.com>
2025-07-24 08:43:39 +0300

    first commit

---
+++ project4Coddy.js
@@ -0,0 +1,15 @@
+let age=20
+let status="Child"
+if (age>18){
+    status="Adult"
+}
+age+=1
+console.log(age)
+console.log(status)
+let x=20
+if (x>=18){
+    status="Adult"
+}else{
+    status="Young"
+}
+
