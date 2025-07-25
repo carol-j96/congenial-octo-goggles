e340f79f554a216b649715136b00cd24b7f6e1df
Caroline Joyce <carol96joyce@gmail>
2025-07-25 09:51:25 +0300

    Add the file to my repo

---
+++ trueOrFalseCoddy.js
@@ -0,0 +1,13 @@
+const readline=require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+rl.question("Enter the number: ", function (number){
+    number=parseInt(number)
+    if (number=1){
+        console.log("T")
+    }else {
+        console.log("F")
+    }
+})
